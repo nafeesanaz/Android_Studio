85410222fac661a1bec47ac26a8d0746c33f0b66
nafeesanaz <naznafisa53@gmail.com>
2021-02-20 16:13:52 +0000

    adding image

--- AndroidApplication/app/src/main/java/com/example/androidapplication/ScreenItem.java
+++ AndroidApplication/app/src/main/java/com/example/androidapplication/ScreenItem.java
@@ -1,5 +1,32 @@
 package com.example.androidapplication;
 
 public class ScreenItem {
+    String Title, Description;
+    int ScreenImg;
 
+    public ScreenItem(String title, String description, int screenImg){
+        Title = title;
+        Description = description;
+        ScreenImg = screenImg;
+    }
+
+    public void setTitle(String title){
+        Title = title;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public int getScreenImg() {
+        return ScreenImg;
+    }
 }
